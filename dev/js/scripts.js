d9d4e2bf9f29fa3d33f8e0501824715abefe3092
sdb329 <sdb329@drexel.edu>
2022-01-17 11:59:58 -0500

    scrolltrigger done!!!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -20,11 +20,34 @@
 
 
 
-import { scrollAnimation } from "./scrollAnimation"
+import { scrollAnimation, scrollAnimation2, scrollAnimationButton1 } from "./scrollAnimation"
 
 window.addEventListener('load', function(){
+    // let regionElements = ["#region-1, #region-2, #region-3, #region-4, #region-5, #region-6,"];
 
-    scrollAnimation()
-    
+    scrollAnimation("#region-1", "#region-1");
+
+    scrollAnimation("#region-2", "#region-2");
+
+    scrollAnimation("#region-3", "#region-3");
+
+    scrollAnimation("#region-4", "#region-4");
+
+    scrollAnimation("#region-5", "#region-5");
+
+    scrollAnimation("#region-6", "#region-6");
+
+
+
+    scrollAnimation2("#visit-1", "#visit-1");
+    scrollAnimation2("#visit-2", "#visit-2");
+    scrollAnimation2("#visit-3", "#visit-3");
+
+    scrollAnimationButton1("#ds-button1", "#ds-button1");
+    scrollAnimationButton1("#ds-button2", "#ds-button2");
+    scrollAnimationButton1("#ds-button3", "#ds-button3");
+    scrollAnimationButton1("#ds-button4", "#ds-button4");
+    scrollAnimationButton1("#ds-button5", "#ds-button5");
+
 });
 
--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -5,24 +5,91 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 
-export function scrollAnimation(){
+export function scrollAnimation(animationElement, triggerElement){
     console.log("this is working test123");
 
     const tl = new gsap.timeline();
 
-    tl.from(".beach",{
-        duration: 6,
-        // x: "+=150",
-        alpha: 0,
-        stagger: 0.4,
-        ease: "back",
-        scrollTrigger: {
-            trigger: "#region-1",
-            toggleActions: "restart none resume none",
-            markers: true,
-            scrub: true
-        }
-    });
+
+    tl.fromTo(animationElement,
+    
+        { 
+            alpha: 0
+        },
+        
+        
+        {
+                // duration: 6,
+                alpha: 1,
+       
+                scrollTrigger: {
+                    trigger: triggerElement,
+                    start: "top 40%",
+                    end: "bottom 80%",
+                    toggleActions: "restart none resume none",
+                    scrub: true
+                }
+            });
 
 }
+
+export function scrollAnimation2(animationElement, triggerElement){
+    console.log("this is working test123");
+
+    const tl = new gsap.timeline();
+
+
+    tl.fromTo(animationElement,
+    
+        { 
+            alpha: 0
+        },
+        
+        
+        {
+                // duration: 6,
+                alpha: 1,
+                x: "-=60",
+       
+                scrollTrigger: {
+                    trigger: triggerElement,
+                    start: "top 50%",
+                    end: "bottom 70%",
+                    toggleActions: "restart none resume none",
+                    scrub: true
+                }
+            });
+
+}
+
+export function scrollAnimationButton1(animationElement, triggerElement){
+    console.log("this is working test123");
+
+    const tl = new gsap.timeline();
+
+
+    tl.fromTo(animationElement,
+    
+        { 
+            alpha: 0,
+            y: "-=50"
+        },
+        
+        
+        {
+                // duration: 6,
+                alpha: 1,
+                y: "+=50",
+                scrollTrigger: {
+                    trigger: triggerElement,
+                    start: "top 60%",
+                    end: "bottom 70%",
+                    toggleActions: "restart none resume none",
+                    scrub: true
+                }
+            });
+
+}
+
+
 
