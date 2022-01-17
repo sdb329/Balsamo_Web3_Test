7ffc4fc4a15a84223e1a1d711a01de9c78376d40
sdb329 <sdb329@drexel.edu>
2022-01-17 08:43:36 -0500

    scrolltrigger working

--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -1,29 +1,30 @@
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 gsap.registerPlugin(ScrollTrigger);
 
 
-
+console.log("hello");
 
 export function usingScroll(){
     console.log("this is working test123");
 
     const tl = new gsap.timeline();
-
-    tl.from("#hero",{
+    tl.from("#hero-image",{
         duration: 6,
         // x: "+=150",
         alpha: 0,
         // stagger: 0.4,
         ease: "back",
         scrollTrigger: {
-            trigger: "#hero-image",
+            trigger: "#hero",
             toggleActions: "restart none resume none",
             markers: true,
             scrub: true
         }
     });
+
 
 }
 
